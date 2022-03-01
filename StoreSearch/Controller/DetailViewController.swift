d8b6f6bffa87995ee41e15ba53a8bad613e8856c
iStef <stefan_b_k@icloud.com>
2022-03-01 16:09:47 +0200

    Close on tap outside pop-up

--- StoreSearch/Controller/DetailViewController.swift
+++ StoreSearch/Controller/DetailViewController.swift
@@ -3,7 +3,7 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
+  
   @IBOutlet weak var popupView: UIView!
   @IBOutlet weak var artworkImageView: UIImageView!
   @IBOutlet weak var nameLabel: UILabel!
@@ -13,14 +13,25 @@ class DetailViewController: UIViewController {
   @IBOutlet weak var priceButton: UIButton!
   
   override func viewDidLoad() {
-        super.viewDidLoad()
-    popupView.layer.cornerRadius = 10
-    }
+    super.viewDidLoad()
     
+    popupView.layer.cornerRadius = 10
+    
+    let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(close))
+    gestureRecognizer.cancelsTouchesInView = false
+    gestureRecognizer.delegate = self
+    view.addGestureRecognizer(gestureRecognizer)
+    
+  }
+  
   @IBAction func close() {
     dismiss(animated: true)
   }
   
+}
 
-
+extension DetailViewController: UIGestureRecognizerDelegate {
+  func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+    return (touch.view === self.view)
+  }
 }
